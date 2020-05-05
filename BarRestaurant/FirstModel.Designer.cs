13ed17d9955cb6948aeb74a8d801a11b03ea6547
lcd97 <danycordero9@gmail.com>
2020-05-05 15:01:38 -0600

    - Second Commit -

--- BarRestaurant/FirstModel.Designer.cs
+++ BarRestaurant/FirstModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Daniela Cordero\Downloads\BarRestaurant\FirstModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Daniela Cordero\Downloads\BarRestaurantControl\BarRestaurant\FirstModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
